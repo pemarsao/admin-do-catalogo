0375c348b418c433e335cc563f4bad7e7fb6462a
Pedro Marsao <pedro.marsao@gmail.com>
2025-12-03 05:51:07 -0300

    Adicionando Observabilidade

--- sandbox/run.sh
+++ sandbox/run.sh
@@ -1,12 +1,15 @@
 # criar as docker networks necessárias
 docker network create adm_videos_services
+docker network create elastic
 
 # Criar as pastas com permissões corretas
 mkdir -m 777 .docker
 mkdir -m 777 .docker/keycloak
+mkdir -m 777 .docker/es01
 
 docker compose -f services/docker-compose.yml up -d
-docker compose -f app/docker-compose.yml up -d
+docker compose -f elk/docker-compose.yml up -d
+#docker compose -f app/docker-compose.yml up -d
 
 echo "Inicializando os containers..."
 sleep 20
