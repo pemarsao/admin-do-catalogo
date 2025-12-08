6898cbef21d7f83e45bc2f1e08da2f073abb5165
Pedro Marsão <pedro.marsao@gmail.com>
2025-12-08 06:28:47 -0300

    Adicionando Observabilidade (#4)
    * Adicionando Observabilidade
    
    * Adicionando Observabilidade
    
    * Adicionando Observabilidade
    
    * Adicionando Observabilidade
    
    * Adicionando Observabilidade

--- sandbox/run.sh
+++ sandbox/run.sh
@@ -1,12 +1,17 @@
 # criar as docker networks necessárias
 docker network create adm_videos_services
+docker network create elastic
 
 # Criar as pastas com permissões corretas
+sudo chown root app/filebeat/filebeat.docker.yml
 mkdir -m 777 .docker
 mkdir -m 777 .docker/keycloak
+mkdir -m 777 .docker/es01
+mkdir -m 777 .docker/filebeat
 
 docker compose -f services/docker-compose.yml up -d
-docker compose -f app/docker-compose.yml up -d
+docker compose -f elk/docker-compose.yml up -d
+#docker compose -f app/docker-compose.yml up -d
 
 echo "Inicializando os containers..."
 sleep 20
