771ddc2d81d698ce30816b8a6a38a04a7d753111
Pedro Marsao <pedro.marsao@gmail.com>
2025-12-09 20:52:47 -0300

    Adicionando Observabilidade fix file

--- sandbox/run.sh
+++ sandbox/run.sh
@@ -8,6 +8,7 @@ mkdir -m 777 .docker
 mkdir -m 777 .docker/keycloak
 mkdir -m 777 .docker/es01
 mkdir -m 777 .docker/filebeat
+sudo chmod go-w app/filebeat/filebeat.docker.yml
 
 docker compose -f services/docker-compose.yml up -d
 docker compose -f elk/docker-compose.yml up -d
